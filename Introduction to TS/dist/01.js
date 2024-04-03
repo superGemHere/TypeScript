f15db2fe5f065a445cec22058a0398f9504b9919
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-04-03 22:42:05 +0300

    01

---
+++ Introduction to TS/dist/01.js
@@ -0,0 +1,4 @@
+function getRectanlgeArea(int1, int2) {
+    console.log(int1 * int2);
+}
+getRectanlgeArea(5, 7);
---
+++ Introduction to TS/src/01.ts
@@ -0,0 +1,4 @@
+function getRectanlgeArea(int1:number, int2:number): void{
+    console.log(int1 * int2)
+}
+getRectanlgeArea(5, 7)
