1a956d855302015e7b3ddadb046e3f0f1dd96e41
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-04-08 19:58:33 +0300

    +6

--- 02 Advanced Types/dist/06.js
+++ 02 Advanced Types/dist/06.js
@@ -1,15 +1,30 @@
-const getCheapestProduct = (arr) => {
-    let myObjArr = [];
+function getCheapestProduct(arr) {
+    let productsCollection = {};
     arr.forEach((item) => {
+        let currentProduct;
         let [town, product, price] = item.split(" | ");
-        let currentTown;
-        currentTown = {
-            productName: product,
-            productTown: town,
-            productPrice: Number(price)
+        currentProduct = {
+            town,
+            price: Number(price)
         };
+        if (!productsCollection[product]) {
+            productsCollection[product] = currentProduct;
+        }
+        else {
+            if (productsCollection[product].price > Number(price)) {
+                productsCollection[product] = currentProduct;
+            }
+        }
     });
-    console.log(myObjArr);
-};
-getCheapestProduct(['Sample Town | Sample Product | 1000', 'Sample Town | Orange | 2', 'Sample Town | Peach | 1', 'Sofia | Orange | 3', 'Sofia | Peach | 2', 'New York | Sample Product | 1000.1', 'New York | Burger | 10']);
+    for (const key in productsCollection) {
+        console.log(`${key} -> ${productsCollection[key].price} (${productsCollection[key].town})`);
+    }
+}
+getCheapestProduct(['Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10']);
 //# sourceMappingURL=06.js.map
--- 02 Advanced Types/src/06.ts
+++ 02 Advanced Types/src/06.ts
@@ -1,22 +1,42 @@
-type ProductBestPrice = {
-    productName: string,
-    productTown: string,
-    productPrice: number,
+type ProductObj  = {
+    product: product;
 }
+type product = {
+town: string;
+price: number;
+}
+type myObj = {
+    name?: product;
+};
 
-const getCheapestProduct = (arr: string[]): void => {
-    let myObjArr: object = {};
-    arr.forEach((item: string) => {
+function getCheapestProduct (arr: string[]): void{
+    let productsCollection: myObj = {};
+    arr.forEach((item: string): void => {
+        let currentProduct: product;
         let [town, product, price] = item.split(" | ");
-
-        let currentTown: ProductBestPrice = {
-            productName: product,
-            productTown: town,
-            productPrice: Number(price)
-       };
-       
+        currentProduct = {
+            town, 
+            price:Number(price)
+        };
+        
+        if(!productsCollection[product]){
+            productsCollection[product] = currentProduct; 
+        }else{
+            if(productsCollection[product].price > Number(price)){
+                productsCollection[product] = currentProduct;
+            }
+        }
     });
-    console.log(myObjArr)
+    for(const key in productsCollection){
+        console.log(`${key} -> ${productsCollection[key].price} (${productsCollection[key].town})`);
+    }
 }
 
-getCheapestProduct(['Sample Town | Sample Product | 1000', 'Sample Town | Orange | 2', 'Sample Town | Peach | 1', 'Sofia | Orange | 3', 'Sofia | Peach | 2', 'New York | Sample Product | 1000.1', 'New York | Burger | 10']) 
+getCheapestProduct(
+    ['Sample Town | Sample Product | 1000', 
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10']) 
