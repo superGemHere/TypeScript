833ae718b57907e78538a440297ea8923d011e3d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-04-14 20:39:45 +0300

    + 04

---
+++ 04 DP & Generics/src/04.ts
@@ -0,0 +1,74 @@
+abstract class CreateAccount<T, U> {
+    bankName: T;
+    bankID: U;
+
+    constructor(bankName: T, bankID: U) {
+        this.bankName = bankName;
+        this.bankID = bankID;
+    }
+}
+
+class PersonalAccount<T, U> extends CreateAccount<T, U> {
+    ownerName: string;
+    money: number;
+    recentTransactions: { [key: string]: number };
+
+    constructor(bankName: T, bankID: U, ownerName: string) {
+        super(bankName, bankID);
+        this.ownerName = ownerName;
+        this.money = 0;
+        this.recentTransactions = {};
+    }
+
+    deposit(amount: number): void {
+        this.money += amount;
+    }
+
+    expense(amount: number, expenseType: string): void {
+        if (this.money >= amount) {
+            if (this.recentTransactions.hasOwnProperty(expenseType)) {
+                this.recentTransactions[expenseType] += amount;
+            } else {
+                this.recentTransactions[expenseType] = amount;
+            }
+            this.money -= amount;
+        } else {
+            throw new Error(`You can't make ${expenseType} transaction`);
+        }
+    }
+
+    calculateTotalMoneySpentOnExpenses(): number {
+        let total = 0;
+        for (const key in this.recentTransactions) {
+            if (Object.hasOwnProperty.call(this.recentTransactions, key)) {
+                total += this.recentTransactions[key];
+            }
+        }
+        return total;
+    }
+
+    showDetails(): string {
+        const totalMoneySpentOnExpenses = this.calculateTotalMoneySpentOnExpenses();
+        return `Bank name: ${this.bankName}\nBank ID: ${this.bankID}\nOwner name: ${this.ownerName}\nMoney: ${this.money}\nMoney spent: ${totalMoneySpentOnExpenses}`;
+    }
+}
+
+let account = new PersonalAccount('DSK', 101240, 'Ivan Ivanov');
+account.deposit(1000);
+account.deposit(1000);
+account.expense(700, 'Buy new phone');
+account.expense(400, 'Book a vacation');
+account.expense(400, 'Book a vacation');
+account.expense(100, 'Buy food');
+console.log(account.showDetails());
+
+console.log("-------------")
+
+let account2 = new PersonalAccount('Fibank', 100000, 'Petar Petrol');
+account2.deposit(10000);
+account2.deposit(7000);
+account2.expense(12000, 'Buy a new car');
+account2.expense(200, 'Go to a fancy restaurant');
+account2.expense(100, 'Go to a bar');
+account2.expense(30, 'Go to the movies');
+console.log(account2.showDetails());
