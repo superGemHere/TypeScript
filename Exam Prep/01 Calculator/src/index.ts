ff777095c692e36ba3a8f43df6cfcc0f386cd72d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2024-04-26 11:26:17 +0300

    01

---
+++ Exam Prep/01 Calculator/src/index.ts
@@ -0,0 +1,33 @@
+function calc(firstNum: number, operator: string, secondNum: number): void{
+    let sum: number | string = null;
+    switch (operator) {
+        case "+":
+            sum = (firstNum + secondNum).toFixed(2)
+            break;
+        case "-":
+            sum = (firstNum - secondNum).toFixed(2)
+            break;
+        case "/":
+            sum = (firstNum / secondNum).toFixed(2)
+            break;
+        case "*":
+            sum = (firstNum * secondNum).toFixed(2)
+            break;
+    
+        default:
+            sum = 'There is not provided operation with that operator.'
+            break;
+    }
+
+    console.log(sum)
+}
+
+calc(5, "+", 10)
+console.log('--------------')
+calc(25.5, "-", 3)
+console.log('--------------')
+calc(9, "/",2)
+console.log('--------------')
+calc(7, "*", 5)
+console.log('--------------')
+calc(7, "asd", 5)
